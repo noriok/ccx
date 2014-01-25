7f0f60753457364f3e3a3df785f4985e7797e981
noriok <noriokob@gmail.com>
2014-01-25 15:19:54 +0900

    changelog.txtの文字コードをUTF8に変更

---
+++ a.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define For(i,x) for (int i=0; i<(int)(x); i++)
+
+int main() {
+    puts("foo");
+    For(i, 10)
+        puts("foo");
+}
